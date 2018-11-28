690a74f0a8b95d1d1b42beef854ef0c4e4c18080
Jonah Simpson <jonahs99@gmail.com>
2018-11-28 17:27:39 -0500

    add snake folder

---
+++ Phase2/GameCentre/app/src/main/java/group_0661/gamecentre/snake/SnakeBoard.java
@@ -0,0 +1,7 @@
+package group_0661.gamecentre.snake;
+
+public class SnakeBoard {
+
+
+
+}
---
+++ Phase2/GameCentre/app/src/main/java/group_0661/gamecentre/snake/SnakeGame.java
@@ -0,0 +1,9 @@
+package group_0661.gamecentre.snake;
+
+import group_0661.gamecentre.gameSystem.Game;
+
+public class SnakeGame extends Game {
+
+
+
+}
