e0be6c2311dfea73cd84619305cae27c0e33483c
Jay Parthasarthy <jparthasarthy@gmail.com>
2018-11-30 11:44:49 -0500

    add SnakeGame.makemove()

--- Phase2/GameCentre/app/src/main/java/group_0661/gamecentre/snake/SnakeGame.java
+++ Phase2/GameCentre/app/src/main/java/group_0661/gamecentre/snake/SnakeGame.java
@@ -39,4 +39,24 @@ public class SnakeGame extends Game implements Serializable{
     public int getScore() {
         return board.getScore();
     }
+
+    public boolean makeMove(String direction) {
+        if (direction.equals("up")) {
+            int[] move = {0, 1};
+            return board.makeMove(move);
+        }
+        if (direction.equals("down")) {
+            int[] move = {0, -1};
+            return board.makeMove(move);
+        }
+        if (direction.equals("right")) {
+            int[] move = {1, 0};
+            return board.makeMove(move);
+        }
+        if (direction.equals("left")) {
+            int[] move = {-1, 0};
+            return board.makeMove(move);
+        }
+        return false;
+    }
 }
