c97dc909083f4740110a26c3abd7610a371ab0c5
Jay Parthasarthy <jparthasarthy@gmail.com>
2018-11-30 12:08:45 -0500

    add Javadoc for SnakeGame

--- Phase2/GameCentre/app/src/main/java/group_0661/gamecentre/snake/SnakeGame.java
+++ Phase2/GameCentre/app/src/main/java/group_0661/gamecentre/snake/SnakeGame.java
@@ -8,10 +8,13 @@ import group_0661.gamecentre.gameSystem.IGame;
 import android.util.Pair;
 
 /**
- * The game class.
+ * The game class for Snake.
  */
 public class SnakeGame extends Game implements Serializable{
 
+    /**
+     * The
+     */
     private SnakeBoard board;
 
 
@@ -31,14 +34,6 @@ public class SnakeGame extends Game implements Serializable{
         return success;
     }
 
-    public boolean swipeMove(String position) {
-            board.makeMove(position);
-            setChanged();
-            notifyObservers();
-            return true;
-        }
-        return false;
-    }
     public boolean isWon() {
         return board.isOver();
     }
